b4ef1138f615dbcda696a47c5d50ac1d673a8b5c
juan david rios <juandavid3196@gmail.com>
2024-10-22 10:59:25 -0500

    first commit

---
+++ api/send-email.js
@@ -0,0 +1,34 @@
+// api/send-email.js
+
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Método no permitido' });
+    }
+
+    const { name, email, message } = req.body;
+
+    // Configuración del transportador de nodemailer
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_LOCAL, 
+        subject: `Nuevo mensaje de ${name}`,
+        text: message,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return res.status(200).json({ message: 'Email enviado' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Error al enviar el email', error });
+    }
+}
