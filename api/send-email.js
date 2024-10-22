c39f4374ccf7863e57ab3d8cb1bc54edf2671e86
juan david rios <juandavid3196@gmail.com>
2024-10-22 11:44:22 -0500

    new error message

--- api/send-email.js
+++ api/send-email.js
@@ -29,6 +29,8 @@ export default async function handler(req, res) {
         await transporter.sendMail(mailOptions);
         return res.status(200).json({ message: 'Email enviado' });
     } catch (error) {
+        console.error('Error al enviar el email:', error);
         return res.status(500).json({ message: 'Error al enviar el email', error });
     }
+    
 }
